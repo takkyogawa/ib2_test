959b0b88940ce91de815ecf902f9911749ecce30
takkyogawa <takkyogawa@gmail.com>
2013-08-15 13:53:48 +0900

    initial comittment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ib2Test::Application.config.secret_token = 'a74aa8d25561fd4fd4676b833443b283a1c2af4945a7f706a7ecc76dd640e92f9111b6090073130101ab813da64b955483db353a47c22faa0b32c5569483186a'
